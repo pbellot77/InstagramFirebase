38b263b43382aef8206392f47975e3f68f5980ca
Patrick <patrickbellot77@gmail.com>
2018-02-15 11:04:10 -0600

    add UIView extension for constraints

---
+++ InstagramFirebase/Utils/Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Extensions.swift
+//  InstagramFirebase
+//
+//  Created by Patrick Bellot on 2/15/18.
+//  Copyright © 2018 Polestar Interactive LLC. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+	
+	static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+		return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+	}
+}
+
