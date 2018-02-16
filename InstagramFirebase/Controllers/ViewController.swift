bcdd05a642a45953e6c5309e78eae6254b8d5afa
Patrick <patrickbellot77@gmail.com>
2018-02-16 11:49:37 -0600

    add profile image

--- InstagramFirebase/Controllers/ViewController.swift
+++ InstagramFirebase/Controllers/ViewController.swift
@@ -9,13 +9,38 @@
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
 	let plusPhotoButton: UIButton = {
 		let button = UIButton(type: .system)
 		button.setImage(#imageLiteral(resourceName: "plus_photo"), for: .normal)
+		button.addTarget(self, action: #selector(handlePlusPhoto), for: .touchUpInside)
 		return button
 	}()
+	
+	@objc func handlePlusPhoto() {
+		let imagePickerController = UIImagePickerController()
+		imagePickerController.delegate = self
+		imagePickerController.allowsEditing = true
+		
+		present(imagePickerController, animated: true, completion: nil)
+	}
+	
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+		
+		if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+			plusPhotoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+		} else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+			plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+		}
+		
+		plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width/2
+		plusPhotoButton.layer.masksToBounds = true
+		plusPhotoButton.layer.borderColor = UIColor.black.cgColor
+		plusPhotoButton.layer.borderWidth = 3
+		
+		dismiss(animated: true, completion: nil)
+	}
 	
 	let emailTextField: UITextField = {
 		let tf = UITextField()
@@ -23,7 +48,6 @@ class ViewController: UIViewController {
 		tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
 		tf.borderStyle = .roundedRect
 		tf.font = UIFont.systemFont(ofSize: 14)
-		
 		tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
 		return tf
 	}()
@@ -69,7 +93,6 @@ class ViewController: UIViewController {
 		button.layer.cornerRadius = 5
 		button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
 		button.setTitleColor(.white, for: .normal)
-		
 		button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
 		
 		button.isEnabled = false
@@ -80,12 +103,38 @@ class ViewController: UIViewController {
 		guard let email = emailTextField.text, email.count > 0 else { return }
 		guard let username = usernameTextField.text, username.count > 0 else { return }
 		guard let password = passwordTextField.text, password.count > 0 else { return }
+		
 		Auth.auth().createUser(withEmail: email, password: password) { (user: User?, error: Error?) in
 			if let err = error {
 				print("Failed to create user:", err)
 				return
 			}
 			print("Successfully created user:", user?.uid ?? "")
+			
+			guard let image = self.plusPhotoButton.imageView?.image else { return }
+			guard let uploadData = UIImageJPEGRepresentation(image, 0.3) else { return }
+			
+			let filename = UUID().uuidString.lowercased()
+			Storage.storage().reference().child("profile_images").child(filename).putData(uploadData, metadata: nil, completion: { (metadata, err) in
+				if let err = err {
+					print("Failed to upload profile image:", err)
+				}
+				guard let profileImageUrl = metadata?.downloadURL()?.absoluteURL else { return }
+				
+				print("Successfully uploaded profile image:", profileImageUrl)
+				
+				guard let uid = user?.uid else { return }
+				let dictionaryValues: Dictionary<String, Any> = ["username": username, "profileImageUrl": profileImageUrl]
+				let values = [uid: dictionaryValues]
+				
+				Database.database().reference().child("users").updateChildValues(values, withCompletionBlock: { (err, ref) in
+					if let err = error {
+						print("Failed to save user info into db:", err)
+					}
+					print("Successfully saved user info to db")
+				})
+			})
+			
 		}
 	}
 	
