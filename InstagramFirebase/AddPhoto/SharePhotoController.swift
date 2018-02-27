7e71e8ce7d3a0b1060c0ca5e59dc2e5b852f5bbe
Patrick <patrickbellot77@gmail.com>
2018-02-27 12:21:37 -0600

    add post to storage and DB

--- InstagramFirebase/AddPhoto/SharePhotoController.swift
+++ InstagramFirebase/AddPhoto/SharePhotoController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SharePhotoController: UIViewController {
 	
@@ -55,7 +56,46 @@ class SharePhotoController: UIViewController {
 	}
 	
 	@objc func handleShare() {
-		print("sharing photo")
+		guard let caption = textView.text, caption.count > 0 else { return }
+		guard let image = selectedImage else { return }
+		
+		guard let uploadData = UIImageJPEGRepresentation(image, 0.5) else { return }
+		
+		navigationItem.rightBarButtonItem?.isEnabled = false
+		
+		let filename = UUID().uuidString
+		Storage.storage().reference().child("posts").child(filename).putData(uploadData, metadata: nil) { (metadata, err) in
+			
+			if let err = err {
+				self.navigationItem.rightBarButtonItem?.isEnabled = true
+				print("Failed to upload post image:", err)
+				return
+			}
+			guard let imageUrl = metadata?.downloadURL()?.absoluteString else { return }
+			print("Successfully uploaded post image:", imageUrl)
+			
+			self.saveToDatabaseWithImageUrl(imageUrl: imageUrl)
+		}
+	}
+	
+	fileprivate func saveToDatabaseWithImageUrl(imageUrl: String) {
+		guard let postImage = selectedImage else { return }
+		guard let caption = textView.text else { return }
+		
+		guard let uid = Auth.auth().currentUser?.uid else { return }
+		
+		let userPostRef = Database.database().reference().child("posts").child(uid)
+		let ref = userPostRef.childByAutoId()
+		let values = ["imageUrl": imageUrl, "caption": caption, "imageWidth": postImage.size.width, "imageHeight": postImage.size.height, "creationDate": Date().timeIntervalSince1970] as [String: Any]
+		ref.updateChildValues(values) { (err, ref) in
+			if let err = err {
+				self.navigationItem.rightBarButtonItem?.isEnabled = true
+				print("Failed to save post to DB", err)
+				return
+			}
+			print("Successfully saved post to DB")
+			self.dismiss(animated: true, completion: nil)
+		}
 	}
 	
 	override var prefersStatusBarHidden: Bool {
