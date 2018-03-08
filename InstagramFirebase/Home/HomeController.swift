43de486fc7f524b20168a2a4cb167621a7b43bd4
Patrick <patrickbellot77@gmail.com>
2018-03-08 13:43:31 -0600

    add following users posts

--- InstagramFirebase/Home/HomeController.swift
+++ InstagramFirebase/Home/HomeController.swift
@@ -20,6 +20,23 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
 		
 		setupNavigationItems()
 		fetchPosts()
+		fetchFollowingUserIds()
+	}
+	
+	fileprivate func fetchFollowingUserIds() {
+		guard let uid = Auth.auth().currentUser?.uid else { return }
+		Database.database().reference().child("following").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
+	
+			guard let userIdsDictionary = snapshot.value as? [String: Any] else { return }
+			
+			userIdsDictionary.forEach({ (key, value) in
+				Database.fetchUserWithUID(uid: key, completion: { (user) in
+					self.fetchPostsWithUser(user: user)
+				})
+			})
+		}) { (err) in
+			print("Failded to fetch following user ids:", err)
+		}
 	}
 	
 	private func setupNavigationItems() {
@@ -46,6 +63,10 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
 				
 				let post = Post(user: user, dictionary: dictionary)
 				self.posts.append(post)
+			})
+			
+			self.posts.sort(by: { (p1, p2) -> Bool in
+				return p1.creationDate.compare(p2.creationDate) == .orderedDescending
 			})
 			self.collectionView?.reloadData()
 			
