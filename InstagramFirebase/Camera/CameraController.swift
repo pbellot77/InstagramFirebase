363385af3b7cb600c4627552459edb8c032334ea
Patrick <patrickbellot77@gmail.com>
2018-03-12 16:49:36 -0500

    Capture photo

--- InstagramFirebase/Camera/CameraController.swift
+++ InstagramFirebase/Camera/CameraController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AVFoundation
 
-class CameraController: UIViewController {
+class CameraController: UIViewController, AVCapturePhotoCaptureDelegate {
 	
 	let dismissButton: UIButton = {
 		let button = UIButton(type: .system)
@@ -28,11 +28,7 @@ class CameraController: UIViewController {
 		button.addTarget(self, action: #selector(handleCapturePhoto), for: .touchUpInside)
 		return button
 	}()
-	
-	@objc func handleCapturePhoto() {
-		print("Capturing photo...")
-	}
-	
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
@@ -48,7 +44,31 @@ class CameraController: UIViewController {
 		view.addSubview(dismissButton)
 		dismissButton.anchor(top: view.topAnchor, left: nil, bottom: nil, right: view.rightAnchor, paddingTop: 15, paddingLeft: 0, paddingBottom: 0, paddingRight: 12, width: 50, height: 50)
 	}
+
+	@objc func handleCapturePhoto() {
+		print("Capturing photo...")
+		
+		let settings = AVCapturePhotoSettings()
+		
+		guard let previewFormatType = settings.availablePreviewPhotoPixelFormatTypes.first else { return }
+		settings.previewPhotoFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewFormatType]
+		
+		output.capturePhoto(with: settings, delegate: self)
+
+	}
 	
+	func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+		guard let imageData = photo.fileDataRepresentation() else { return }
+		let previewImage = UIImage(data: imageData)
+		
+		let previewImageView = UIImageView(image: previewImage)
+		view.addSubview(previewImageView)
+		previewImageView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+		
+		print("Finish processing photo sample buffer...")
+	}
+	
+	let output = AVCapturePhotoOutput()
 	fileprivate func setupCaptureSession() {
 		let captureSession = AVCaptureSession()
 		
@@ -64,7 +84,7 @@ class CameraController: UIViewController {
 			print("Could not setup camera input:", err)
 		}
 		//2. setup outputs
-		let output = AVCapturePhotoOutput()
+		
 		if captureSession.canAddOutput(output) {
 			captureSession.addOutput(output)
 		}
