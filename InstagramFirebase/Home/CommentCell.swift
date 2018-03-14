42c21c6ad51ae5c7568350003e8bdd4e7a18460c
Patrick <patrickbellot77@gmail.com>
2018-03-14 15:56:10 -0500

    add auto size comment cells

--- InstagramFirebase/Home/CommentCell.swift
+++ InstagramFirebase/Home/CommentCell.swift
@@ -12,23 +12,39 @@ class CommentCell: UICollectionViewCell {
 	
 	var comment: Comment? {
 		didSet {
-			textLabel.text = comment?.text
+			guard let comment = comment else { return }
+			
+			let attributedText = NSMutableAttributedString(string: comment.user.username, attributes: [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 14)])
+			attributedText.append(NSAttributedString(string: " " + comment.text, attributes: [NSAttributedStringKey.foregroundColor: UIColor.black, .font: UIFont.systemFont(ofSize: 14)]))
+			
+			textView.attributedText = attributedText
+			profileImageView.loadImage(urlString: comment.user.profileImageUrl)
 		}
 	}
 	
-	let textLabel: UILabel = {
-		let label = UILabel()
-		label.font = UIFont.systemFont(ofSize: 14)
-		label.numberOfLines = 0
-		label.backgroundColor = .lightGray
-		return label
+	let textView: UITextView = {
+		let textView = UITextView()
+		textView.font = UIFont.systemFont(ofSize: 14)
+		textView.isScrollEnabled = false
+		return textView
+	}()
+	
+	let profileImageView: CustomImageView = {
+		let iv = CustomImageView()
+		iv.clipsToBounds = true
+		iv.contentMode = .scaleAspectFill
+		iv.layer.cornerRadius = 20
+		return iv
 	}()
 	
 	override init(frame: CGRect) {
 		super.init(frame: frame)
 		
-		addSubview(textLabel)
-		textLabel.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 4, paddingLeft: 4, paddingBottom: 4, paddingRight: 4, width: 0, height: 0)
+		addSubview(profileImageView)
+		profileImageView.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 8, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 40, height: 40)
+		
+		addSubview(textView)
+		textView.anchor(top: topAnchor, left: profileImageView.rightAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 4, paddingLeft: 4, paddingBottom: 4, paddingRight: 4, width: 0, height: 0)
 	}
 	
 	required init?(coder aDecoder: NSCoder) {
--- InstagramFirebase/Home/CommentsController.swift
+++ InstagramFirebase/Home/CommentsController.swift
@@ -20,7 +20,9 @@ class CommentsController: UICollectionViewController, UICollectionViewDelegateFl
 		
 		navigationItem.title = "Comments"
 		
-		collectionView?.backgroundColor = .blue
+		collectionView?.backgroundColor = .white
+		collectionView?.alwaysBounceVertical = true
+		collectionView?.keyboardDismissMode = .interactive
 		collectionView?.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: -50, right: 0)
 		collectionView?.scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: -50, right: 0)
 		
@@ -36,19 +38,34 @@ class CommentsController: UICollectionViewController, UICollectionViewDelegateFl
 		ref.observe(.childAdded, with: { (snapshot) in
 			
 			guard let dictionary = snapshot.value as? [String: Any] else { return }
+			guard let uid = dictionary["uid"] as? String else { return }
 			
-			let comment = Comment(dictionary: dictionary)
-			
-			self.comments.append(comment)
-			self.collectionView?.reloadData()
-			
+			Database.fetchUserWithUID(uid: uid, completion: { (user) in
+				let comment = Comment(user: user, dictionary: dictionary)
+				self.comments.append(comment)
+				self.collectionView?.reloadData()
+			})
 		}) { (err) in
 			print("Failed to observe comments")
 		}
 	}
 	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+		return 0
+	}
+	
 	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-		return CGSize(width: view.frame.width, height: 50)
+		
+		let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 50)
+		let dummyCell = CommentCell(frame: frame)
+		dummyCell.comment = comments[indexPath.item]
+		dummyCell.layoutIfNeeded()
+		
+		let targetSize = CGSize(width: view.frame.width, height: 1000)
+		let estimatedSize = dummyCell.systemLayoutSizeFitting(targetSize)
+		
+		let height = max(40 + 8 + 8, estimatedSize.height)
+		return CGSize(width: view.frame.width, height: height)
 	}
 	
 	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -88,6 +105,11 @@ class CommentsController: UICollectionViewController, UICollectionViewDelegateFl
 		
 		containerView.addSubview(commentTextField)
 		commentTextField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: submitButton.leftAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+		
+		let lineSeparatorView = UIView()
+		lineSeparatorView.backgroundColor = UIColor.rgb(red: 230, green: 230, blue: 230)
+		containerView.addSubview(lineSeparatorView)
+		lineSeparatorView.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: nil, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0.5)
 		
 		return containerView
 	}()
